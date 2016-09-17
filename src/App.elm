7f2c044f66201e68745a9fce10f534704bde0a6f
Matt Weldon <matt.weldon@gmail.com>
2016-09-17 12:22:21 +0100

    update app module to use navigation.program and hook everything together

--- src/App.elm
+++ src/App.elm
@@ -1,7 +1,31 @@
 module App exposing (..)
 
 import Html exposing (..)
+import Navigation exposing (..)
+import Hop.Types exposing (Address)
 
-main : Html msg
+import Models exposing (..)
+import Routes exposing (..)
+import Routing exposing (..)
+
+main : Program Never
 main =
+  Navigation.program Routing.urlParser
+    { init = init
+    , urlUpdate = Routing.urlUpdate
+    , view = view
+    , update = update
+    , subscriptions = (always Sub.none)
+    }
+
+init : ( Route, Address ) -> ( Model, Cmd msg )
+init ( route, address ) =
+    ( Model address route, Cmd.none )
+
+view : Model -> Html msg
+view model =
   h1 [] [ text "Hello World" ]
+
+update : msg -> Model -> ( Model, Cmd msg )
+update msg model =
+  ( model, Cmd.none )
