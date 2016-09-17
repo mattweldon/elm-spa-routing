528c388fb7ebefd0b747b1d7e9e8e2175855e27d
Matt Weldon <matt.weldon@gmail.com>
2016-09-17 12:39:17 +0100

    add nav buttons to our app

--- src/Updates.elm
+++ src/Updates.elm
@@ -1,7 +1,19 @@
 module Updates exposing (..)
 
 import Models exposing (..)
+import Routing exposing (..)
 
-update : msg -> Model -> ( Model, Cmd msg )
+type Msg
+  = NavigateToHome
+  | NavigateToAbout
+  | NavigateTo404
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  ( model, Cmd.none )
+  case msg of
+    NavigateToHome ->
+      ( model, navigateTo "" )
+    NavigateToAbout ->
+      ( model, navigateTo "/about" )
+    NavigateTo404 ->
+      ( model, navigateTo "asdf" )
--- src/Views.elm
+++ src/Views.elm
@@ -1,16 +1,25 @@
 module Views exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
 
 import Models exposing (..)
 import Routes exposing (..)
+import Updates exposing (..)
 
-render : Model -> Html msg
+render : Model -> Html Msg
 render model =
-  case model.route of
-    HomeRoute ->
-      h1 [] [ text "Home" ]
-    AboutRoute ->
-      h1 [] [ text "About" ]
-    NotFoundRoute ->
-      h1 [] [ text "404" ]
+  div []
+    [ div []
+      [ button [ onClick (NavigateToHome) ] [ text "Home" ]
+      , button [ onClick (NavigateToAbout) ] [ text "About" ]
+      , button [ onClick (NavigateTo404) ] [ text "404" ]
+      ]
+    , case model.route of
+        HomeRoute ->
+          h1 [] [ text "Home" ]
+        AboutRoute ->
+          h1 [] [ text "About" ]
+        NotFoundRoute ->
+          h1 [] [ text "404" ]
+    ]
